9f7131d8109aaef48d5c9a3972f183d673e92aa1
oscarmasson <oscarmasson@yahoo.com>
2024-02-24 18:46:59 -0500

    asdf

---
+++ app/footer/footer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector:'app-footer',
+    templateUrl: './footer.Component.html',
+    styleUrls:['footer.Component.css']
+})
+export class FooterComponent {
+    autor:any = {nombre:'Oscar',apellido:'Masson'}
+}
